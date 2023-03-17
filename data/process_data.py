90190573b306c39aa6ebee82c381c315e911b108
dirkules339 <dirk.lindner@uni-dortmund.de>
2023-03-17 11:00:20 +0100

    changed process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,36 @@
 import sys
-
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv('disaster_messages.csv')
+    categories = pd.read_csv('disaster_categories.csv')
+    df = messages.merge(categories, on='id')
+    return df
 
 
 def clean_data(df):
-    pass
+    categories =  df['categories'].str.split(pat=';', expand=True)
+    
+    row = categories.iloc[[1]]
+    category_colnames = [category_name.split('-')[0] for category_name in row.values[0]]
+    categories.columns = category_colnames
+
+    for column in categories:
+    categories[column] = categories[column].astype(str).str[-1:]
+    categories[column] = categories[column].astype(int)
+
+    df.drop(['categories'], axis=1, inplace=True)
+    df = pd.concat([df,categories], join='inner', axis=1)
+    df.drop_duplicates(inplace=True)
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///disaster_response_db.db')
+    df.to_sql('disaster_response_table', engine, index=False,if_exists='replace')
 
 
 def main():
@@ -36,7 +56,7 @@ def main():
               'well as the filepath of the database to save the cleaned data '\
               'to as the third argument. \n\nExample: python process_data.py '\
               'disaster_messages.csv disaster_categories.csv '\
-              'DisasterResponse.db')
+              'disaster_response_db.db')
 
 
 if __name__ == '__main__':
